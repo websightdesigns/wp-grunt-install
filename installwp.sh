3887a03ea354d310b96cedf230ff2dbe418a898e
Ezra Free <ezrafree@gmail.com>
2018-03-26 10:34:07 -0600

    update htaccess filename

--- installwp.sh
+++ installwp.sh
@@ -124,7 +124,7 @@ PHP
 	find . -type f -name '.DS_Store' -exec rm -v {} \;
 
 	# copy over files
-	cp -v $skeletonfiles/.htaccess .
+	cp -v $skeletonfiles/.htaccess.sample .htaccess
 	cp -v $skeletonfiles/gruntfile.js .
 	cp -v $skeletonfiles/package.json .
 	cp -v $skeletonfiles/wp-cli.yml .
